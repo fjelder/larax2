f5d906192813f36bcaa02890c6eccf64f251691f
fjelder <42210053+fjelder@users.noreply.github.com>
2022-04-01 13:59:36 +0200

    work 0104 - pagination

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -23,14 +23,6 @@
               fill-rule="evenodd" clip-rule="evenodd"></path>
           </svg>
         </button>
-
-        <div class="btn">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-          </svg>
-
-        </div>
         <!-- Settings Dropdown -->
         <div class="relative">
           <x-jet-dropdown align="right" width="48">
